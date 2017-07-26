29a3b318a66c78b97645b066ff179ebc0e723342
Aluno <danrley.tri@gmail.com>
2017-07-26 10:43:51 -0300

    teste programa

---
+++ Teste/src/Janela.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Janela extends JFrame {
+	
+	public Janela() {
+		
+		super("Testando Eclipse");
+		Container tela = getContentPane();
+		setLayout(null);
+		
+		setSize(400,400);
+		setVisible(true);
+	}
+	
+}
---
+++ Teste/src/Principal.java
@@ -0,0 +1,11 @@
+
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class Principal {
+
+	public static void main(String[]Args) {
+		Janela app = new Janela();
+		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	} 
+}
