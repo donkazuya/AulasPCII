d09a734ccab549d5df6002043815868c7c5958fc
Aluno <danrley.tri@gmail.com>
2017-07-26 10:53:28 -0300

    pjMaven

---
+++ projetopcii/src/main/java/etec/pcii/projetopcii/App.java
@@ -0,0 +1,13 @@
+package etec.pcii.projetopcii;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
