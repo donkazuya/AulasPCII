c2373bd6557505617784b98b49fdd6e4872f4782
Danrley <danrley.tri@gmail.com>
2017-08-31 13:42:58 -0300

    Implantação da classe view Aluno

--- testeConexao/src/etec/pcii/projetopcii/model/Aluno.java
+++ testeConexao/src/etec/pcii/projetopcii/model/Aluno.java
@@ -3,11 +3,21 @@ package etec.pcii.projetopcii.model;
 public class Aluno extends Pessoa {
 
 	private String nomeAluno;
+	private int rm;
 	
-	public Aluno(String nomeCompleto, String nomeAluno){
+	public Aluno(String nomeCompleto, String nomeAluno,int rm){
 		super(nomeCompleto);
 		this.nomeAluno = nomeAluno;
+		this.rm = rm;
 		
+	}
+
+	public int getRm() {
+		return rm;
+	}
+
+	public void setRm(int rm) {
+		this.rm = rm;
 	}
 
 	public String getNomeAluno() {
---
+++ testeConexao/src/etec/pcii/projetopcii/swing/view/AlunoPanel.java
@@ -0,0 +1,25 @@
+package etec.pcii.projetopcii.swing.view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+
+public class AlunoPanel extends JFrame {
+	
+	private JLabel titulo = new JLabel("Alunos: ");
+	private JLabel lblNomeAluno = new JLabel ("Nome do aluno: ");
+	private JLabel lblRm = new JLabel("RM: ");
+	private JTextField txtNomeAluno = new JTextField("",100);
+	private JTextField txtRm = new JTextField(10);
+	
+	public AlunoPanel() {
+		super ("Area Aluno");
+		Container tela = getContentPane();
+		
+		
+		
+	}
+
+}
